73c8ecdf719d74b9de514e508df36d91ccaa352b
Grygoriy Zholtkevych <g.zholtkevych@karazin.ua>
2016-07-08 23:05:17 +0300

    Improving functions inc and dec

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def is_correct(cval, nvals):
         cval:    int                checked value
         nvals:   int  {nvals >= 2}  upper bound of the range
 
-        return:  int | TypeError | ValueError
+        return:  bool | TypeError | ValueError
     """
     if not isinstance(cval, int):
         return TypeError("type of 'cval' is invalid")
@@ -17,19 +17,40 @@ def is_correct(cval, nvals):
     if nvals < 2:
         return ValueError("value of 'nvals' is invalid")
     if cval < 0 or cval >= nvals:
-        return TypeError("value of 'cval' is invalid")
-    return 0
+        return ValueError("value of 'cval' is invalid")
+    return True
 
 
 def inc(cval, nvals):
-    res = is_correct(cval, nvals)
-    if not isinstance(res, int): raise res
+    """The function increments value of 'cval' keeping the condition
+        cval < nvals
+
+    Arguments:
+    ----------
+        cval:   int   {0 <= cval < nvals}  increased value
+        nvals:  int   {nvals >= 2}         upper bound of the range
+
+        return:  int  increased value
+    """
+    result = is_correct(cval, nvals)
+    if isinstance(result, Exception):  # checking is not successful
+        raise result
     cval += 1
     return min(cval, nvals - 1)
 
 
 def dec(cval, nvals):
-    res = is_correct(cval, nvals)
-    if not isinstance(res, int): raise res
+    """The function decrements value of 'cval' keeping the condition
+        cval >= 0
+
+    Arguments:
+    ----------
+        cval:   int   {0 <= cval < nvals}  decreased value
+        nvals:  int   {nvals >= 2}         upper bound of the range
+
+        return:  int  decreased value
+    """
+    result = is_correct(cval, nvals)
+    if isinstance(result, Exception): raise result
     cval -= 1
     return max(0, cval)
